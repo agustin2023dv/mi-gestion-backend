2a027120c36978db958bcf5131095b6f2481625a
agustin <contacto.agustin32@gmail.com>
2026-04-14 19:46:05 +0200

    chore: add application-dev.yml with cloud service placeholders

---
+++ src/main/java/com/migestion/MiGestionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.migestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiGestionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiGestionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/migestion/MiGestionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiGestionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
